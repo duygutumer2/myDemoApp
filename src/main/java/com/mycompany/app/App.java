bbecf92855583ef71ccd9b4e31f8cdc404871475
duygutumer2 <duygutumer1993@gmail.com>
2024-03-08 14:16:12 +0300

    getting ready for deployment to heroku

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -74,6 +74,15 @@ public class App {
         else return -1;
 
     }
+    public static boolean search(ArrayList<Integer> array, int e) {
+        System.out.println("inside search");
+        if (array == null) return false;
+        for (int elt : array) {
+            if (elt == e) return true;
+        }
+        return false;
+    }
+
 
     public static boolean isPalindrome(String str){
         String cleanStr = str.replaceAll("[^a-zA-Z]", "").toLowerCase();
@@ -86,7 +95,44 @@ public class App {
         return true;
     }
 
-
+    public static void main(String[] args) {
+        port(getHerokuAssignedPort());
+        get("/", (req, res) -> "Hello, World");
+        post("/compute", (req, res) -> {
+//System.out.println(req.queryParams("input1"));
+//System.out.println(req.queryParams("input2"));
+            String input1 = req.queryParams("input1");
+            java.util.Scanner sc1 = new java.util.Scanner(input1);
+            sc1.useDelimiter("[;\r\n]+");
+            java.util.ArrayList<Integer> inputList = new java.util.ArrayList<>();
+            while (sc1.hasNext())
+            {
+                int value = Integer.parseInt(sc1.next().replaceAll("\\s",""));
+                inputList.add(value);
+            }
+            System.out.println(inputList);
+            String input2 = req.queryParams("input2").replaceAll("\\s","");
+            int input2AsInt = Integer.parseInt(input2);
+            boolean result = App.search(inputList, input2AsInt);
+            Map map = new HashMap();
+            map.put("result", result);
+            return new ModelAndView(map, "compute.mustache");
+        }, new MustacheTemplateEngine());
+        get("/compute",
+                (rq, rs) -> {
+                    Map map = new HashMap();
+                    map.put("result", "not computed yet!");
+                    return new ModelAndView(map, "compute.mustache");
+                },
+                new MustacheTemplateEngine());
+    }
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
 }
 
 
